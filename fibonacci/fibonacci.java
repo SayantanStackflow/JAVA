f270ff9dfa4103c7fa61a90fe1db84793e9cfbfe
SayantanStackflow <sayantan.college6291@gmail.com>
2022-08-16 21:39:04 +0100

    fiboanacci series using constractor

---
+++ fibonacci/fibonacci.java
@@ -0,0 +1,15 @@
+//fibonacci series using constractor
+public class fibonacci{
+    public static void main(String args[]) {
+        int a1=0, a2=1, a3, i, count= 10;
+        System.out.println(a1+" "+ a2);
+        for(i=1; i<count; i++)
+        {
+            a3= a1+a2;
+            System.out.println(" \r"+ a3);
+            a1= a2;
+            a2=a3;   
+        }
+        
+    }
+}
